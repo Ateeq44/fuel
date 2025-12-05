3ae872fcdbc5d4ee7eb1084a443655d169f9d8cb
ateeq44 <ateeqrehman4479@gmail.com>
2025-12-06 01:03:47 +0500

    df

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return redirect()->route('user_fuel_entries.index');
+})->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
